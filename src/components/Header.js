4bd00ec3f284f4bac6046acb1d2e9285522130d5
MujuniEli <elijahmujuni@gmail.com>
2022-12-08 21:26:22 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+
+import trollface from "../images/trollface-shades.png"
+
+export default function Header() {
+    return (
+
+            <header className="header">
+                    <img src={trollface} alt="navigation icon" className="header--image"/>
+                    <h2 className="header--title">Meme Generator</h2>
+                    <h4 className="header--projects">React Course - Project 3</h4>
+            </header>
+    )
+}
