62d543af1319e1a2a0b2cb9c77a38855bf8a3813
MujuniEli <elijahmujuni@gmail.com>
2022-12-20 21:55:26 +0300

    get random image

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,12 +1,26 @@
+import { useState } from "react"
+import memesData from "../memesData"
+
 
 export default function Meme() {
+
+    const [memeImage, setMemeImage] = useState("")
+
+    function getMeme() {
+        const memesArray = memesData.data.memes 
+        const randomNumber = Math.floor(Math.random() * memesArray.length) 
+        setMemeImage (memesArray[randomNumber].url) 
+        
+    }
+
     return (
                 <main>
-                    <form className="form">
+                    <div className="form">
                     <input type="text" placeholder="Top text" className="form--input"/>
                     <input type="text" placeholder="Bottom text" className="form--input"/>
-                    <button className="form--button">Get a new meme image</button>
-                    </form>
+                    <button onClick={getMeme} className="form--button">Get a new meme image</button>
+                    <img src={memeImage} alt="random pic" className="meme--image" />
+                    </div>
                 </main>
     )
 }
