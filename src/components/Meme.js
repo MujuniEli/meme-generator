498c926063442c29e4d26da5d5d88ee45eabcf16
MujuniEli <elijahmujuni@gmail.com>
2023-01-07 12:51:56 +0300

    add effect

--- src/components/Meme.js
+++ src/components/Meme.js
@@ -1,5 +1,5 @@
-import { useState } from "react"
-import memesData from "../memesData"
+import { useEffect, useState } from "react"
+// import memesData from "../memesData"
 
 
 export default function Meme() {
@@ -10,12 +10,18 @@ export default function Meme() {
         randomImage: "http://i.imgflip.com/1bij.jpg"
     })
 
-    const [allMemeImages, setAllMemeImages] = useState(memesData)
+    const [allMemeImages, setAllMemeImages] = useState([])
+
+    useEffect(()=> {
+        fetch("https://api.imgflip.com/get_memes")
+        .then(res => res.json())
+        .then(data => setAllMemeImages(data.data.memes))
+    })
 
     function getMeme() {
-        const memesArray = allMemeImages.data.memes 
-        const randomNumber = Math.floor(Math.random() * memesArray.length) 
-        const url = memesArray[randomNumber].url
+        
+        const randomNumber = Math.floor(Math.random() * allMemeImages.length) 
+        const url = allMemeImages[randomNumber].url
 
         setMeme(prevMeme => ({
                 ...prevMeme,
